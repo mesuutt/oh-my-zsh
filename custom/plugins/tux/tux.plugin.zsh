687e5de2eed160e5446be5601d5a60424149d0c3
Mesut Tasci <slackermesut@gmail.com>
2013-05-17 12:53:32 +0300

    History completion improved

--- custom/plugins/tux/tux.plugin.zsh
+++ custom/plugins/tux/tux.plugin.zsh
@@ -14,3 +14,9 @@ tup() {
 export TEAMOCIL_PATH=$TMUX_PATH/teamocil/
 compctl -g '~/.tmux/teamocil/*(:t:r)' teamocil
 
+
+# Search in history not only command, all words. Look: http://superuser.com/questions/417627/oh-my-zsh-history-completion
+# If doesn't work for the up or down arrows, press <ctrl-v><KEY (e.g., up arrow)>
+bindkey '^[OA' history-beginning-search-backward
+bindkey '^[OB' history-beginning-search-forward
+
