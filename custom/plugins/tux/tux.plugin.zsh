0824689260c8434c826f0e4d0f82c895f39e83b0
Mesut Tasci <slackermesut@gmail.com>
2013-07-25 05:02:39 +0300

    New aliases added

--- custom/plugins/tux/tux.plugin.zsh
+++ custom/plugins/tux/tux.plugin.zsh
@@ -6,10 +6,28 @@ sudu() { su -c "'$@'";};
 alias h='howdoi'
 alias http='noglob http'
 alias git='noglob git'
+alias c='clear'
 
 
 # Search in history not only command, all words. Look: http://superuser.com/questions/417627/oh-my-zsh-history-completion
 # If doesn't work for the up or down arrows, press <ctrl-v><KEY (e.g., up arrow)>
 bindkey '^[OA' history-beginning-search-backward
 bindkey '^[OB' history-beginning-search-forward
+
+
+alias dmesgc='dmesg | ccze -A'
+alias dt='date +%H:%M:%S\ %Y-%m-%d\ %a\ %b | ccze -A'
+psc () { /bin/ps $@ | ccze -A; }
+tail () { /usr/bin/tail $@ | ccze -A; }
+t () { /usr/bin/tail -f * | ccze -A; }
+ant () { /usr/bin/ant $@ | ccze -A ; }
+
+# Backup file before edit
+vims() { cp {,`date +%Y%m%d`_}$*; vim "$*"; }
+
+# # Remove unnecessary lines from file
+sedc(){ sed -e '/^#/d' -e 's/#.*$//' $1; }
+
+# # Quickly cd out Directories : up 2 == cd ../../
+up() { local x='';for i in $(seq ${1:-1});do x="$x../"; done;cd $x; }
 
