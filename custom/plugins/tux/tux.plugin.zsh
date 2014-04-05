40f729ef593023c0bd21e7e68bd0a3cbfbe7ba8c
Mesut Tasci <slackermesut@gmail.com>
2014-04-06 02:01:01 +0300

    tux plugin updated

--- custom/plugins/tux/tux.plugin.zsh
+++ custom/plugins/tux/tux.plugin.zsh
@@ -1,22 +1,8 @@
 alias http='noglob http'
 alias git='noglob git'
 alias find='noglob find'
-
-alias c='clear'
 alias h='howdoi'
-
-# Search in history not only command, all words. Look: http://superuser.com/questions/417627/oh-my-zsh-history-completion
-# If doesn't work for the up or down arrows, press <ctrl-v><KEY (e.g., up arrow)>
-bindkey '^[OA' history-beginning-search-backward
-bindkey '^[OB' history-beginning-search-forward
-
-
 alias dmesgc='dmesg | ccze -A'
-alias dt='date +%H:%M:%S\ %Y-%m-%d\ %a\ %b | ccze -A'
-psc () { /bin/ps $@ | ccze -A; }
-tail () { /usr/bin/tail $@ | ccze -A; }
-t () { /usr/bin/tail -f * | ccze -A; }
-ant () { /usr/bin/ant $@ | ccze -A ; }
 
 # Backup file before edit
 vims() { cp {,`date +%Y%m%d`_}$*; vim "$*"; }
